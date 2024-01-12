b8fff158d72689e06797828683cf1dce35f6acfb
Hamza Ashfaq <hamza2.idenbrid@gmail.com>
2024-01-13 01:46:51 +0500

    changes done

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const UserController = require('./controllers/userController')
-const CompanyController = require('./controllers/companyController')
-const CategoryController = require('./controllers/categoryController')
-const ReviewController = require('./controllers/reviewController')
+const UserController = require("./controllers/userController");
+const CompanyController = require("./controllers/companyController");
+const CategoryController = require("./controllers/categoryController");
+const ReviewController = require("./controllers/reviewController");
 
-const { register, login } = require('./controllers/authController')
+const { register, login } = require("./controllers/authController");
 
-app.use(express.json())
+app.use(express.json());
 // const deployedUrl = 'https://mern-e-commerce-project.vercel.app'
 // const corsOptions = {
 //   origin: deployedUrl,
 // };
-app.use(cors())
+app.use(cors());
 
-app.use('/signup', register)
-app.use('/login', login)
-app.use('/user', UserController)
-app.use('/company', CompanyController)
-app.use('/reviews', ReviewController)
-app.use('/category', CategoryController)
+app.use("/", (req, res) => {
+  return res.status(200).send("Welcome to review app");
+});
 
-module.exports = app
+app.use("/signup", register);
+app.use("/login", login);
+app.use("/user", UserController);
+app.use("/company", CompanyController);
+app.use("/reviews", ReviewController);
+app.use("/category", CategoryController);
+
+module.exports = app;
