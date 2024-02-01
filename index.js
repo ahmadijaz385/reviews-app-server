7146bd2147f4c8d95ba20458f7dddae002e8631e
Hamza Ashfaq <hamza2.idenbrid@gmail.com>
2024-02-01 17:05:55 +0500

    changes done

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const User = require('./models/userModel')
+const Company = require('./models/companyModel')
+const Review = require('./models/reviewModel')
+
 const UserController = require('./controllers/userController')
 const CompanyController = require('./controllers/companyController')
 const CategoryController = require('./controllers/categoryController')
@@ -25,5 +29,16 @@ app.use('/company', CompanyController)
 app.use('/reviews', ReviewController)
 app.use('/category', CategoryController)
 app.use('/inbox', InboxController)
+
+app.get('/count', async (req, res) => {
+	try {
+		const user = await User.countDocuments()
+		const company = await Company.countDocuments()
+		const reviews = await Review.countDocuments()
+		return res.status(200).send({ user, company, reviews })
+	} catch (error) {
+		return res.status(500).send({ error: error.message })
+	}
+})
 
 module.exports = app
